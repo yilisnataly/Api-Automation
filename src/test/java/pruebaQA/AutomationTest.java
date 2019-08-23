d333ed66c9435dc040cd23466a0cfc2eb7185300
Nataly <yilis.nataly@gmail.com>
2019-08-23 13:31:19 +0200

    Maybe response doesn't match

---
+++ src/test/java/pruebaQA/AutomationTest.java
@@ -0,0 +1,94 @@
+package pruebaQA;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class AutomationTest {
+
+
+
+    @Test
+    public void requestDecision_checkStatusCode () {
+
+        given().
+                param("yes").
+                param("no").
+                param("maybe").
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void requestDecision_checkContentType() {
+
+        given().
+                param("yes").
+                param("no").
+                param("maybe").
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                assertThat().
+                contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void requestDecision_logRequest() {
+
+        given().
+                param("yes").
+                param("no").
+                param("maybe").
+                log().all().
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                log().body();
+
+    }
+
+    @Test
+    public void requestDecision_checkAnswerInResponseBody_ExpectYes() {
+
+        given().
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                assertThat().
+                body("answer", equalToObject("yes"));
+
+    }
+
+    @Test
+    public void requestDecision_checkAnswerInResponseBody_ExpectNo() {
+
+        given().
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                assertThat().
+                body("answer", equalTo("no"));
+
+    }
+
+    @Test
+    public void requestDecision_checkAnswerInResponseBody_ExpectMaybe() {
+
+        given().
+                when().
+                get("https://yesno.wtf/api").
+                then().
+                assertThat().
+                body("answer", equalTo("maybe"));
+
+    }
+}
+
